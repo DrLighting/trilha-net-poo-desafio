55a22f59553921743e1e8cae033f02f19734a68f
Daniel Nakano <dnakano@alunos.utfpr.edu.br>
2025-07-14 23:14:49 -0300

    Implementado POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("------Smartphone Nokia------");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "3310", imei: "123456789", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("------Smartphone Iphone------");
+Smartphone iphone = new Iphone(numero: "5678", modelo: "9", imei: "987654321", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
